502ea1b081cc1132ad158ea4ca0491412ca5eb07
Janitha Nawarathna <64242868+s16417@users.noreply.github.com>
2021-07-28 12:44:15 +0530

    Changes url for tagged posts in post detail component

--- src/components/post/PostDetailComponent.js
+++ src/components/post/PostDetailComponent.js
@@ -126,7 +126,7 @@ export default function PostDetail() {
 
         const RenderPostTags = postTags ? postTags.map((tag, key) => 
             <Grid item key={key} >
-                <Chip className={classes.chip} color="primary" size="small" variant="outlined" label={tag} key={key} component="a" href={`/questions/tagged/${tag}`} clickable/>
+                <Chip className={classes.chip} color="primary" size="small" variant="outlined" label={tag} key={key} component="a" href={`/tagged/${tag}`} clickable/>
             </Grid>): [];
 
         return(
